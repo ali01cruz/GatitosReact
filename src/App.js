bc96e893c5b542ac0b0c1a1c428835c6b5d6f214
Silvana <silvanamilu2@gmail.com>
2021-10-11 16:39:50 -0300

    footer falta links de redes

--- src/App.js
+++ src/App.js
@@ -2,43 +2,32 @@ import './App.css';
 import {Switch,Route} from "react-router-dom";
 import NavbarComp from './components/navbarcomp/navbarcomp';
 import About from './components/about/about';
+import Footer from './components/footer/footer';
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
      
         <NavbarComp/>
         <Switch>
+          
           <Route exact path="/" >
                       <h1>HOLA</h1>
+                      <Footer/>
           </Route>
+
           <Route exact path="/about" >
                       <About/>
+                      <Footer/>
           </Route>
+
           <Route>
               <h1> PAG NOT FOUND 404</h1>
           </Route>
         </Switch>
 
   
->>>>>>> main
     </div>
   );
 }
