ea8b0969b7a062a7c938d50c4faeae0558a66b91
ali <ali01cruz@gmail.com>
2021-10-12 22:05:51 -0300

    dee

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,27 +1,28 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import {Form} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { useForm } from 'react-hook-form'
 const HookForm = () => {
+    const {register ,errors, handleSubmit}= useForm();
+
     const styleform={
         maxWidth: "500px",
         margin: 'auto',
         backgroundColor:'var(--color1)',
         padding:'1%'
     };
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
 
     return (
         <div className="Hookform" style ={styleform}>
-            <Form>
-                <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
-                </Form.Group>
-            </Form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("firstName", { required: true, maxLength: 20 })} />
+                <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                <input type="number" {...register("age", { min: 18, max: 99 })} />
+                <input type="submit" />
+            </form>
         </div >
     );
 }
